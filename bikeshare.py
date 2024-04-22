f965a33eeed3f5bbc8857a1f81b497f518cb2743
Abelasu Valenteen <l.abelashvalenteen@gmail.com>
2024-04-22 16:21:30 +0200

    fix: code style warnings fixed

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,8 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bike share data!')
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid
+    # inputs
     city = input("Please enter a city name: ").lower()
     while city not in CITY_DATA:
         city = input("Oh sorry, Please choose from these three cities (chicago, new york city, washington): ").lower()
@@ -164,7 +165,7 @@ def raw_data(df):
        """
     print("Enter to display row data, no to skip.")
     x = 0
-    while (input() != 'no'):
+    while input() != 'no':
         x = x + 5
         print(df[x:x + 5])
 
